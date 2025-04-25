cbc7d1870022db0db1288f6b9128688532aff17e
Jaehyeon Han <jaehyeonhan99@gmail.com>
2025-04-25 14:40:35 +0900

    refactor: extract Init class

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -5,19 +5,12 @@ import hello.domain.MenuItem;
 
 import java.util.List;
 
+import static hello.init.Init.initMenuItems;
+
 public class Main {
     public static void main(String[] args) {
         List<MenuItem> menuItems = initMenuItems();
         Kiosk kiosk = new Kiosk(menuItems);
         kiosk.start();
-    }
-
-    private static List<MenuItem> initMenuItems() {
-        MenuItem shackBurger = new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem smokeShack = new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem cheeseBurger = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem hamburger = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
-
-        return List.of(shackBurger, smokeShack, cheeseBurger, hamburger);
     }
 }
