2e8b4212e90c623a8d67f19da01f6b0ca5f748e4
Jaehyeon Han <jaehyeonhan99@gmail.com>
2025-04-28 15:06:01 +0900

    refacotr(Init): remove unnecessary initBurgerMenu()

--- src/main/java/hello/init/Init.java
+++ src/main/java/hello/init/Init.java
@@ -10,15 +10,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Init {
-    private static List<MenuItem> initBurgerMenu() {
-        MenuItem shackBurger = new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem smokeShack = new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem cheeseBurger = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem hamburger = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
-
-        return List.of(shackBurger, smokeShack, cheeseBurger, hamburger);
-    }
-
     public static List<Menu> initMenus() {
         try (BufferedReader br = new BufferedReader(new FileReader(new File("./src/main/resources/menu.data")))) {
             List<Menu> menus = new ArrayList<>();
